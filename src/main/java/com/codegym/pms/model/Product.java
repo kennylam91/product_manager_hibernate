47f78521b331fb9a94b7d77b9d1001fb85c5c9e5
kennylam91 <phamvanlam1991@gmail.com>
2019-07-04 17:25:22 +0700

    add Product class in model package

---
+++ src/main/java/com/codegym/pms/model/Product.java
@@ -0,0 +1,25 @@
+package com.codegym.pms.model;
+
+public class Product {
+    private String name;
+    private long price;
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+}
