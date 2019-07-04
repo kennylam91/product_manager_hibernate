d039c2072773742962a386c2418a30ba37193f1d
kennylam91 <phamvanlam1991@gmail.com>
2019-07-04 17:53:51 +0700

    edit Product model

--- src/main/java/com/codegym/pms/model/Product.java
+++ src/main/java/com/codegym/pms/model/Product.java
@@ -1,11 +1,36 @@
 package com.codegym.pms.model;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Product(String name, long price) {
+        this.name = name;
+        this.price = price;
+    }
+
     private String name;
     private long price;
 
     public Product() {
     }
+
+
 
     public String getName() {
         return name;
