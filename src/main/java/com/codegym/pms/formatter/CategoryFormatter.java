b9882c11229a5f0e054b5fe14c25beceda60cc50
kennylam91 <phamvanlam1991@gmail.com>
2019-07-08 11:14:20 +0700

    add CategoryFormatter class

---
+++ src/main/java/com/codegym/pms/formatter/CategoryFormatter.java
@@ -0,0 +1,26 @@
+package com.codegym.pms.formatter;
+
+import com.codegym.pms.model.Category;
+import com.codegym.pms.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+@Component
+public class CategoryFormatter implements Formatter<Category> {
+    @Autowired
+    CategoryService categoryService;
+
+    @Override
+    public Category parse(String text, Locale locale) throws ParseException {
+        return categoryService.findById(Long.parseLong(text));
+    }
+
+    @Override
+    public String print(Category object, Locale locale) {
+        return "[" + object.getId() + ", " +object.getName() + "]";
+    }
+}
